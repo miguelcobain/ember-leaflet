dd28043d9fea9ebab9db9ca9f22ac4cdeaecc3af
Miguel Andrade <jmandrade0@gmail.com>
2015-11-13 20:04:29 +0000

    fix split helper bug

--- tests/dummy/app/helpers/split.js
+++ tests/dummy/app/helpers/split.js
@@ -2,6 +2,6 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(function([arg1 = '', index = 0, separator = ':']) {
-  return arg1.split(separator)
+  return arg1.split(separator)[index];
 });
 /* jshint ignore:end */
