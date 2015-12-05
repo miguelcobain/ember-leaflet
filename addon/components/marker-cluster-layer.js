2d3e8c85a24e9a5fd867d166408f964a8c3f2287
Petr Kosikhin <mq3_storm@mail.ru>
2015-12-05 13:36:28 +0300

    added working meaningfull test for markercluster

--- addon/components/marker-cluster-layer.js
+++ addon/components/marker-cluster-layer.js
@@ -1,21 +1,34 @@
 import Ember from 'ember';
-import layout from '../templates/components/marker-cluster-layer';
 import BaseLayer from 'ember-leaflet/components/base-layer';
 import ContainerMixin from 'ember-leaflet/mixins/container';
+
 /* global L */
 
-export default Ember.Component.extend({
-	layout,
+export default BaseLayer.extend(ContainerMixin,{
 	L,
 	createLayer(){
 		return this.L.markerClusterGroup(...this.get('requiredOptions'), this.get('options'));
 	},
 	didInsertElement() {
-	this._super(...arguments);
-	this.get('_childLayers').invoke('layerSetup');
+		this._super(...arguments);
+		this.layerSetup();
+		this.get('_childLayers').invoke('layerSetup');
 	},
-	willDestroyElement() {
-	this._super(...arguments);
-	this.get('_childLayers').clear();
-	}
+	willDestroyLayer() {
+		this.get('_childLayers').invoke('layerTeardown');
+		this.get('_childLayers').clear();
+	},
+	layerSetup() {
+		if (Ember.isNone(this.get('_layer'))) {
+			this._layer = this.createLayer();
+			this._addObservers();
+			this._addEventListeners();
+			this.didCreateLayer();
+		}
+		if (this.get('containerLayer')) {
+			if (!Ember.isNone(this.get('containerLayer')._layer)) {
+				this.get('containerLayer')._layer.addLayer(this._layer);
+			}
+		}
+    }
 });
--- tests/integration/components/marker-cluster-layer-test.js
+++ tests/integration/components/marker-cluster-layer-test.js
@@ -1,25 +1,45 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import MarkerClusterLayerComponent from 'ember-leaflet/components/marker-cluster-layer';
+import locations from '../../helpers/locations';
+/* globals L */
+
+//Needed to silence leaflet autodetection error
+L.Icon.Default.imagePath = 'some-path';
+
+let cluster;
 
 moduleForComponent('marker-cluster-layer', 'Integration | Component | marker cluster layer', {
-  integration: true
+  integration: true,
+  beforeEach() {
+
+    this.register('component:marker-cluster-layer', MarkerClusterLayerComponent.extend({
+      init() {
+        this._super(...arguments);
+        cluster = this;
+      }
+    }));
+
+    this.set('center', locations.nyc);
+    this.set('zoom', 13);
+  },
+  afterEach() {
+  }
 });
 
 test('it renders', function(assert) {
-  
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  this.set('markerCenter', locations.nyc);
+  this.set('icon', L.divIcon({className: 'my-div-icon'}));
 
-  this.render(hbs`{{marker-cluster-layer}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:" + EOL +
+  // this maxZoom property here is for purpose. Otherwise
+  // Leaflet.markercluster causes some weird behaviour on addlayer.
   this.render(hbs`
-    {{#marker-cluster-layer}}
-      template block text
-    {{/marker-cluster-layer}}
-  `);
+    {{#leaflet-map zoom=zoom center=center maxZoom=25}}
+      {{#marker-cluster-layer}}
+        {{marker-layer location=markerCenter icon=icon}}
+      {{/marker-cluster-layer}}
+    {{/leaflet-map}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(cluster._childLayers.length, 1);  
 });
