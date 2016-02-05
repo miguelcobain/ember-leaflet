00a6ee33a04559eb4bdd2579185f105a0d1a1438
David Jackson <altmail@jackson-wallace.net>
2016-02-05 01:35:12 -0600

    Added popup-layer and accompanying documentation

--- tests/dummy/app/controllers/docs/actions.js
+++ tests/dummy/app/controllers/docs/actions.js
@@ -4,11 +4,20 @@ export default Ember.Controller.extend({
   lat: 45.519743,
   lng: -122.680522,
   zoom: 10,
+  popupLocation: null,
   actions: {
     updateCenter(e) {
       let center = e.target.getCenter();
       this.set('lat', center.lat);
       this.set('lng', center.lng);
+    },
+
+    setPopupLocation(e) {
+      this.set('popupLocation', e.latlng);
+    },
+
+    resetPopupLocation() {
+      this.set('popupLocation', null);
     }
   }
 });
