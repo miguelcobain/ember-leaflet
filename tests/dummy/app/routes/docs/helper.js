1617b2874f187df9fabac435efb30c5a8105bbca
Miguel Andrade <jmandrade0@gmail.com>
2016-03-29 23:45:38 +0100

    create helper example components. refactored a bit

--- tests/dummy/app/routes/docs/helper.js
+++ tests/dummy/app/routes/docs/helper.js
@@ -1,30 +1,16 @@
-/* global require */
-
 import Ember from 'ember';
-
-const helperDescs = {
-  'div-icon':
-    `<p>This helper returns a <a href="http://leafletjs.com/reference.html#divicon"><code>L.DivIcon</code></a> instance.<p>
-     <p>Useful to customize the icon of a <code>marker-layer</code>.</p>`,
-  'icon':
-    `<p>This helper returns a <a href="http://leafletjs.com/reference.html#icon"><code>L.Icon</code></a> instance.</p>
-     <p>Useful to customize the icon of a <code>marker-layer</code>.</p>`,
-  'point':
-    `<p>This helper returns a <a href="http://leafletjs.com/reference.html#point"><code>L.Point</code></a> instance.</p>`
-};
+/* global require */
 
 export default Ember.Route.extend({
   model(params) {
     this.helperName = params.helper_name;
-    this.helperDesc = helperDescs[this.helperName] || 'No description yet.';
     return require(`ember-leaflet/helpers/${params.helper_name}`).default;
   },
 
   setupController(controller, model) {
     controller.setProperties({
-      helperName: this.helperName,
-      helperDesc: this.helperDesc,
-      helperFunc: model.compute.toString(),
+      helper: model,
+      helperName: this.helperName
     });
   }
 });
