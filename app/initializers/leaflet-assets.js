b594b011f67803326749acc66f380fda5ebf0908
indr <mail@indr.ch>
2016-10-13 21:53:32 +0800

    Set icon default imagePath according to  ENV.rootURL, ENV.baseURL

--- app/initializers/leaflet-assets.js
+++ app/initializers/leaflet-assets.js
@@ -1,1 +1,11 @@
-export { default, initialize } from 'ember-leaflet/initializers/leaflet-assets';
+import ENV from '../config/environment';
+/* global L */
+
+export function initialize(/* container, application */) {
+  L.Icon.Default.imagePath = `${ENV.rootURL || ENV.baseURL || '/'}assets/images/`;
+}
+
+export default {
+  name: 'leaflet-assets',
+  initialize: initialize
+};
