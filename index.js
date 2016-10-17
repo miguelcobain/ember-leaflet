b4dc1229ba376c487a2622f4fa52feaa0cad684e
Ilya Radchenko <knownasilya@gmail.com>
2016-10-17 12:37:03 -0400

    fix: nested addon/engines support for asset import

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ var VersionChecker = require('ember-cli-version-checker');
 module.exports = {
   name: 'ember-leaflet',
   included: function(app) {
+   this._super.included.apply(this, arguments);
+
+   // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+   // use that.
+   if (typeof this._findHost === 'function') {
+     app = this._findHost();
+   }
+
+   // Otherwise, we'll use this implementation borrowed from the _findHost()
+   // method in ember-cli.
+   // Keep iterating upward until we don't have a grandparent.
+   // Has to do this grandparent check because at some point we hit the project.
+   var current = this;
+   do {
+     app = current.app || app;
+   } while (current.parent.parent && (current = current.parent));
+    
    //import javascript
    app.import(app.bowerDirectory + '/leaflet/dist/leaflet-src.js');
 
