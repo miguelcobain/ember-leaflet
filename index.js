969d11caa330cb476a3ad5d4341c5129a2344d16
Adam Rekosiewicz <rekosiewicz.adam@gmail.com>
2016-10-31 12:00:50 +0100

    dont include css

--- index.js
+++ index.js
@@ -7,37 +7,47 @@ var VersionChecker = require('ember-cli-version-checker');
 module.exports = {
   name: 'ember-leaflet',
   included: function(app) {
-   this._super.included.apply(this, arguments);
+    this._super.included.apply(this, arguments);
 
-   // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
-   // use that.
-   if (typeof this._findHost === 'function') {
-     app = this._findHost();
-   }
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    }
 
-   // Otherwise, we'll use this implementation borrowed from the _findHost()
-   // method in ember-cli.
-   // Keep iterating upward until we don't have a grandparent.
-   // Has to do this grandparent check because at some point we hit the project.
-   var current = this;
-   do {
-     app = current.app || app;
-   } while (current.parent.parent && (current = current.parent));
-    
-   //import javascript
-   app.import(app.bowerDirectory + '/leaflet/dist/leaflet-src.js');
+    // Otherwise, we'll use this implementation borrowed from the _findHost()
+    // method in ember-cli.
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    var current = this;
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
 
-   app.import(app.bowerDirectory + '/leaflet/dist/leaflet.css');
+    //import javascript
+    app.import(app.bowerDirectory + '/leaflet/dist/leaflet-src.js');
 
-   var imagesDestDir = '/assets/images';
-   app.import(app.bowerDirectory + '/leaflet/dist/images/layers-2x.png', { destDir: imagesDestDir });
-   app.import(app.bowerDirectory + '/leaflet/dist/images/layers.png', { destDir: imagesDestDir });
-   app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon-2x.png', { destDir: imagesDestDir });
-   app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon.png', { destDir: imagesDestDir });
-   app.import(app.bowerDirectory + '/leaflet/dist/images/marker-shadow.png', { destDir: imagesDestDir });
- },
+    //app.import(app.bowerDirectory + '/leaflet/dist/leaflet.css');
 
- treeForAddonTemplates: function treeForAddonTemplates (tree) {
+    var imagesDestDir = '/assets/images';
+    app.import(app.bowerDirectory + '/leaflet/dist/images/layers-2x.png', {
+      destDir: imagesDestDir
+    });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/layers.png', {
+      destDir: imagesDestDir
+    });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon-2x.png', {
+      destDir: imagesDestDir
+    });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon.png', {
+      destDir: imagesDestDir
+    });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-shadow.png', {
+      destDir: imagesDestDir
+    });
+  },
+
+  treeForAddonTemplates: function treeForAddonTemplates(tree) {
     var checker = new VersionChecker(this);
     var dep = checker.for('ember', 'bower');
 
@@ -46,7 +56,9 @@ module.exports = {
     if (dep.lt('2.3.0-beta.1')) {
       var current = this.treeGenerator(path.join(baseTemplatesPath, 'current'))
       var specificVersionTemplate = this.treeGenerator(path.join(baseTemplatesPath, 'lt-2-3'));
-      return mergeTrees([current, specificVersionTemplate], { overwrite: true })
+      return mergeTrees([current, specificVersionTemplate], {
+        overwrite: true
+      })
     } else {
       return this.treeGenerator(path.join(baseTemplatesPath, 'current'));
     }
