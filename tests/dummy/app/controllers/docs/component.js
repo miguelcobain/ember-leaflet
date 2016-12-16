3405022170f4f2bd4d65aef50543a8d959e670ba
Miguel Andrade <jmandrade0@gmail.com>
2016-12-16 12:09:40 +0000

    fix component and property lookup from docs

--- tests/dummy/app/controllers/docs/component.js
+++ tests/dummy/app/controllers/docs/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 const { Controller, computed } = Ember;
 
-let URL_PREFIX = 'http://leafletjs.com/reference.html#';
+let URL_PREFIX = 'http://leafletjs.com/reference-1.0.2.html#';
 
 export default Controller.extend({
 
@@ -38,24 +38,9 @@ export default Controller.extend({
     return false;
   },
 
-  _subtractSuperClassProps(propName) {
-    let component = this.get('component');
-    let props = component.get(propName) ? Ember.A(component.get(propName).slice(0)) : Ember.A();
-    let clazz = component.constructor.superclass.superclass;
-
-    //subtract superclass properties
-    while (clazz !== Ember.Component) {
-      let classProps = clazz.prototype[propName] || [];
-      props.removeObjects(classProps);
-      clazz = clazz.superclass;
-    }
-
-    return props;
-  },
-
   superclassName: computed('component', function() {
     let component = this.get('component');
-    let superclass = component.constructor.superclass.superclass;
+    let superclass = component.superclass.superclass;
     if (superclass !== Ember.Component) {
       return this._findClassSource(superclass).split('/').pop();
     } else {
@@ -65,23 +50,27 @@ export default Controller.extend({
 
   leafletRequiredOptions: computed('component', function() {
     let component = this.get('component');
-    return component.get('leafletRequiredOptions');
+    return component.prototype.leafletRequiredOptions;
   }),
 
   leafletProperties: computed('component', function() {
-    return this._subtractSuperClassProps('leafletProperties');
+    let component = this.get('component');
+    return component.prototype.leafletProperties;
   }),
 
   leafletOptions: computed('component', function() {
-    return this._subtractSuperClassProps('leafletOptions');
+    let component = this.get('component');
+    return component.prototype.leafletOptions;
   }),
 
   leafletEvents: computed('component', function() {
-    return this._subtractSuperClassProps('leafletEvents');
+    let component = this.get('component');
+    return component.prototype.leafletEvents;
   }),
 
   leafletStyleProperties: computed('component', function() {
-    return this._subtractSuperClassProps('leafletStyleProperties');
+    let component = this.get('component');
+    return component.prototype.leafletStyleProperties;
   })
 
 });
--- tests/dummy/app/routes/docs/component.js
+++ tests/dummy/app/routes/docs/component.js
@@ -5,7 +5,7 @@ const { Route, getOwner } = Ember;
 export default Route.extend({
   model(params) {
     this.componentName = params.component_name;
-    return getOwner(this).lookup(`component:${params.component_name}`);
+    return getOwner(this)._lookupFactory(`component:${params.component_name}`);
   },
 
   setupController(controller, model) {
