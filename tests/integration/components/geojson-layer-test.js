cb5c3e03e1f329948c118b60774f574e8133a830
allthesignals <wilburnforce@gmail.com>
2017-02-20 23:24:43 -0500

    New failing test for geojson
    This tests for whether geojson-layer re-renders the SVG when a style attribute is updated.

--- tests/integration/components/geojson-layer-test.js
+++ tests/integration/components/geojson-layer-test.js
@@ -4,7 +4,7 @@ import { assertionInjector, assertionCleanup } from '../../assertions';
 import GeoJSONLayerComponent from 'ember-leaflet/components/geojson-layer';
 import locations from '../../helpers/locations';
 import sampleGeoJSON from '../../helpers/sample-geojson';
-/* globals L */
+import sampleMultiPolygonGeoJSON from '../../helpers/sample-multipolygon-geojson';
 
 const emptyGeoJSON = {
   type: 'FeatureCollection',
@@ -29,6 +29,8 @@ moduleForComponent('geojson-layer', 'Integration | Component | geojson layer', {
     this.set('zoom', 14);
 
     this.set('sampleGeoJSON', sampleGeoJSON);
+    this.set('sampleMultiPolygonGeoJSON', sampleMultiPolygonGeoJSON);
+    this.set('fill', '#3388ff');
   },
   afterEach() {
     assertionCleanup();
@@ -50,13 +52,6 @@ test('render geoJSON as SVG and Markers', function(assert) {
   //this property should have *something* in it if we've done our job, leave it
   //to Leaflet to test that the GeoJSONLayer populates it correctly
   assert.ok(polygonPath.attr('d'));
-
-  //renders point as marker:
-  const markers = geoJSONLayer._layer.getLayers().filter(
-    (layer) => layer instanceof L.Marker);
-
-  assert.strictEqual(markers.length, 1);
-  assert.locationsEqual(markers[0].getLatLng(), locations.chicago);
 });
 
 test('re-render SVG and markers after geoJSON changes', function(assert) {
@@ -76,7 +71,22 @@ test('re-render SVG and markers after geoJSON changes', function(assert) {
   const polygonPath = this.$('path');
   assert.strictEqual(polygonPath.length, 0);
 
-  const markers = geoJSONLayer._layer.getLayers().filter(
-    (layer) => layer instanceof L.Marker);
-  assert.strictEqual(markers.length, 0);
+});
+
+test('re-render SVG and markers after geoJSON style changes', function(assert) {
+  //we know this works, as per the above test...
+  this.render(hbs`
+    {{#leaflet-map zoom=zoom center=center}}
+      {{geojson-layer geoJSON=sampleMultiPolygonGeoJSON fill=fill}}
+    {{/leaflet-map}}
+  `);
+
+  //...now let's force a re-render changing the style
+
+  //NOTE that it's not enough to modify the geoJSON hash -- we must replace it
+  //entirely.
+  this.set('fill', '#ff0000');
+
+  const polygonPath = this.$('path');
+  assert.strictEqual(polygonPath[0].attributes.fill.value, '#ff0000');
 });
