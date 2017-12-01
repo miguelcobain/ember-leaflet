5c7ec673feee378a88a60592718b82e97e91b3eb
Miguel Andrade <jmandrade0@gmail.com>
2017-12-01 05:09:48 +0000

    fix issue with fastboot + overlay layers

--- addon/components/div-overlay-layer.js
+++ addon/components/div-overlay-layer.js
@@ -1,3 +1,5 @@
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 import BaseLayer from 'ember-leaflet/components/base-layer';
 import layout from '../templates/div-overlay';
 import { RenderBlockMixin } from 'ember-composability-tools';
@@ -7,5 +9,14 @@ export default BaseLayer.extend(RenderBlockMixin, {
 
   leafletOptions: [
     'offset', 'className', 'pane'
-  ]
+  ],
+
+  fastboot: computed(function() {
+    let owner = getOwner(this);
+    return owner.lookup('service:fastboot');
+  }),
+
+  isFastBoot: computed('fastboot', function() {
+    return this.get('fastboot') && this.get('fastboot.isFastBoot');
+  })
 });
