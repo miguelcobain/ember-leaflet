e53fd8745470435f8c86e1f3c5002ca6201360c0
Miguel Andrade <jmandrade0@gmail.com>
2019-02-06 17:48:10 +0000

    use proper object

--- addon/helpers/div-icon.js
+++ addon/helpers/div-icon.js
@@ -1,9 +1,12 @@
 import { helper } from '@ember/component/helper';
+import { assign } from '@ember/polyfills';
 const isFastBoot = typeof FastBoot !== 'undefined';
 /* global L */
 
 export const divIcon = isFastBoot ? function() {} : function divIcon(_, hash) {
-  return L.divIcon(hash);
+  // https://github.com/emberjs/ember.js/issues/14668
+  let options = assign({}, hash);
+  return L.divIcon(options);
 };
 
 export default helper(divIcon);
--- addon/helpers/icon.js
+++ addon/helpers/icon.js
@@ -1,9 +1,12 @@
 import { helper } from '@ember/component/helper';
+import { assign } from '@ember/polyfills';
 const isFastBoot = typeof FastBoot !== 'undefined';
 /* global L */
 
 export const icon = isFastBoot ? function() {} : function icon(_, hash) {
-  return L.icon(hash);
+  // https://github.com/emberjs/ember.js/issues/14668
+  let options = assign({}, hash);
+  return L.icon(options);
 };
 
 export default helper(icon);
