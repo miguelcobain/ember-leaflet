d9628b438a162c59cba3018a3b0fd5d595db3e10
Jonathan <jonathan.hudson2012@gmail.com>
2020-01-14 12:33:47 -0500

    Try to resolve testing for older version of leaflet

--- tests/integration/components/rectangle-layer-test.js
+++ tests/integration/components/rectangle-layer-test.js
@@ -34,14 +34,14 @@ module('Integration | Component | rectangle layer', function(hooks) {
     `);
 
     let layerLatLngs = rectangle._layer.getLatLngs(); //returns four corners
-    assert.locationsEqual(layerLatLngs[0][1], locations.chicago); //top left
-    assert.locationsEqual(layerLatLngs[0][3], locations.nyc); //bottom right
+    assert.locationsEqual(layerLatLngs[0][1] || layerLatLngs[1], locations.chicago); //top left
+    assert.locationsEqual(layerLatLngs[0][3] || layerLatLngs[3], locations.nyc); //bottom right
 
     this.set('locations', [locations.paris, locations.london]);
 
     layerLatLngs = rectangle._layer.getLatLngs(); //returns two corners
-    assert.locationsEqual(layerLatLngs[0][0], locations.paris); //top left
-    assert.locationsEqual(layerLatLngs[0][1], locations.london); //bottom right
+    assert.locationsEqual(layerLatLngs[0][0] || layerLatLngs[0], locations.paris); //top left
+    assert.locationsEqual(layerLatLngs[0][1] || layerLatLngs[1], locations.london); //bottom right
   });
 
   test('lat/lng changes propagate to the rectangle layer', async function(assert) {
@@ -62,8 +62,8 @@ module('Integration | Component | rectangle layer', function(hooks) {
     `);
 
     let layerLatLngs = rectangle._layer.getLatLngs(); //returns 4 corners 
-    assert.locationsEqual(layerLatLngs[0][3], locations.nyc); //bottom right
-    assert.locationsEqual(layerLatLngs[0][1], locations.chicago); //top left
+    assert.locationsEqual(layerLatLngs[0][3] || layerLatLngs[3], locations.nyc); //bottom right
+    assert.locationsEqual(layerLatLngs[0][1] || layerLatLngs[1], locations.chicago); //top left
 
     this.setProperties({
       lat1: locations.paris.lat,
@@ -75,7 +75,7 @@ module('Integration | Component | rectangle layer', function(hooks) {
     this.set('locations',[[this.get('lat1'),this.get('lng1')],[this.get('lat2'),this.get('lng2')]])
 
     layerLatLngs = rectangle._layer.getLatLngs(); //returns only two corners
-    assert.locationsEqual(layerLatLngs[0][0], locations.paris); //top left
-    assert.locationsEqual(layerLatLngs[0][1], locations.london); //bottom right
+    assert.locationsEqual(layerLatLngs[0][0] || layerLatLngs[0], locations.paris); //top left
+    assert.locationsEqual(layerLatLngs[0][1] || layerLatLngs[1], locations.london); //bottom right
   });
 });
