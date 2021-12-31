0b2dc85de46d874f32536614d00c1390e345293b
Miguel Andrade <jmandrade0@gmail.com>
2021-12-31 13:01:45 +0000

    update ember-cli project

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,66 +7,66 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.20-leaflet-0.7.7',
+        name: 'ember-lts-3.24-leaflet-0.7.7',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
+            'ember-source': '~3.24.3',
             leaflet: '~0.7.7'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.5'
-          }
-        }
+          },
+        },
       },
       {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.3'
-          }
-        }
+            'ember-source': '~3.24.3',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
       },
       {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
+            'ember-source': await getChannelURL('release'),
+          },
+        },
       },
       {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
       },
       {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
       },
       {
         name: 'ember-default-with-jquery',
         env: {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
+            'jquery-integration': true,
+          }),
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^1.1.0'
-          }
-        }
+            '@ember/jquery': '^1.1.0',
+          },
+        },
       },
       {
         name: 'ember-classic',
@@ -74,20 +74,20 @@ module.exports = async function () {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
             'application-template-wrapper': true,
             'default-async-observers': false,
-            'template-only-glimmer-components': false
-          })
+            'template-only-glimmer-components': false,
+          }),
         },
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0'
+            'ember-source': '~3.28.0',
           },
           ember: {
-            edition: 'classic'
-          }
-        }
+            edition: 'classic',
+          },
+        },
       },
       embroiderSafe(),
-      embroiderOptimized()
-    ]
+      embroiderOptimized(),
+    ],
   };
 };
